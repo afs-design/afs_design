17420fa1d8631b92f7f89147398d44c175a6b3b5
afs-design <afs_info@naver.com>
2026-05-04 15:30:00 +0900

    Update ThemePremium.tsx

--- src/themes/ThemePremium.tsx
+++ src/themes/ThemePremium.tsx
@@ -58,7 +58,7 @@ const projects = [
     details: [
       '/digital/digital_02.png',
       '/digital/digital_03.png',
-      '/digital/digital_04.gif',
+      '/qr.png',
       '/digital/digital_05.png',
       '/digital/digital_06.png',
       '/digital/digital_07.png',
